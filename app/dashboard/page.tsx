9df8ff74958b53ac23330f1b5c824dee5a770fbd
Hot7 <hot7@MacBook-Air.local>
2025-02-25 09:21:58 +0800

    create routes and layout,confirm the ui section

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-2xl md:text-4xl md:not-italic font-bold md:leading-normal">
+        Dashboard
+      </h1>
+    </main>
+  );
+}
