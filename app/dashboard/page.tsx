a567ef67d5f1fafa288c2ab8dabefe9b16cfa09c
hot777zzz <912275990@qq.com>
2025-02-25 11:33:32 +0800

    init database with vercel and fetch data

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <main>
-      <h1 className="text-2xl md:text-4xl md:not-italic font-bold md:leading-normal">
-        Dashboard
-      </h1>
-    </main>
-  );
-}
